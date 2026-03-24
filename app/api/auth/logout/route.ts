cf617b49c6b6b8c121bf50cae279f3667c7c7597
Danyael Vásquez <dandev@WS-ADATOS-DNVAS.getec.cl>
2026-03-24 18:28:51 -0300

    ADD redirect en el cierre de sesion de admin

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { COOKIE_NAME } from '@/lib/auth';
 
 export async function POST(req: Request) {
-  const response = NextResponse.redirect(new URL('/admin', req.url));
+  const response = NextResponse.redirect(new URL('/', req.url));
   response.cookies.delete(COOKIE_NAME);
   return response;
 }
